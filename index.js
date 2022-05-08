a2fee862bd51ab28da910c8c36798a73b7c54874
Satish <sharmasatish2010@gmail.com>
2022-05-08 08:12:48 +0100

    added more comments

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
+// Adding values to an array
 const number = [3, 4];
 number.push(5, 6);
 number.unshift(1, 2);
 number.splice(2, 0, 'a', 'b');
 console.log(number);
-
+// Working with indexes
 const num1 = [1, 2, 3, 1, 4];
 console.log(num1.indexOf(1, 3));
 console.log(num1.lastIndexOf(1));
 console.log(num1.includes(1));
-
+// Array of object
 const courses = [
     { id: 1, name: 'Satish' },
     { id: 2, name: 'Anay' }
 ]
 const course = courses.find(course => course.name === 'Satish')
 console.log(course);
-
+// Delete value from array
 const num2 = [1, 2, 3, 4];
 console.log(num2.pop());
 console.log(num2.shift());
 console.log(num2);
-
+// delete using splice
 const num3 = [1, 2, 3, 4];
 console.log(num3.splice(2, 2));
-
 // Empty an Array
 const num4 = [1, 2, 3, 4];
 num4.length = 0;
 console.log(num4);
-
+// combining array
 const first = [1, 2, 3];
 const second = [4, 5, 6];
 const combined = first.concat(second);
 console.log(combined);
 const slice = combined.slice(2, 4);
 console.log(slice);
-
+// copying an array
 const third = [1, 2, 3];
 const fourth = [4, 5, 6];
 const combinedTwo = [...third, ...fourth];
 console.log(combinedTwo);
 const copy = [...combinedTwo];
 console.log(copy);
-
+// for each loop
 const five = [1, 2, 3, 4];
 five.forEach((five, index) => {
     console.log(five, index);
 })
-
+// join in array
 const num5 = [1, 2, 3]
 const joined = num5.join(',');
 console.log(joined);
-
+// split in array
 const message = "This is my first message";
 const split = message.split(' ');
 console.log(split);
@@ -62,7 +62,7 @@ console.log(joinedTwo);
 const num6 = [1, 3, 2];
 console.log(num6.sort());
 console.log(num6.reverse());
-
+// sorting 
 const sortOne = [
     { id: 1, name: 'Node.js' },
     { id: 2, name: 'JavaScript' }
@@ -75,7 +75,7 @@ sortOne.sort(function compare(a, b) {
     return 0;
 });
 console.log(sortOne);
-
+// every
 const num7 = [1, 2, -2, 3];
 const allPositive = num7.every(function (value) {
     return value >= 0;
@@ -85,7 +85,7 @@ const atLeastOnePositive = num7.some(function (value) {
     return value >= 0;
 })
 console.log(atLeastOnePositive);
-
+// filtered
 const num8 = [1, 2, -2, 3];
 const filtered = num8.filter((value) => value >= 0)
 console.log(filtered);
@@ -104,7 +104,7 @@ const itemsOne = num9
     .map(obj => obj.value);
 console.log(itemsOne);
 
-//Sum of an Array
+//Sum of an Array using reduce
 const num10 = [1, 2, -2, 3];
 const reduceNum = num10.reduce((a, c) => a + c);
 console.log(reduceNum);
